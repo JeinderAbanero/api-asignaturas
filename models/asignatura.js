724d5b0153559a951fb23145634e3078f87e195d
JeinderAbanero <Jeinderabanero@gmail.com>
2024-12-08 22:06:50 -0400

    Correccion de rutas

--- models/asignatura.js
+++ models/asignatura.js
@@ -1,16 +1,37 @@
+// models/asignatura.js
 const mongoose = require("mongoose");
 
 const asignaturaSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  creditos: { type: Number, required: true },
-  profesor: { type: String, required: true },
-  descripcion: String,
-  horario: String,
-  aula: String,
-  prerrequisitos: String,
-  cupoMaximo: { type: Number, required: true },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  creditos: {
+    type: Number,
+    required: true,
+  },
+  profesor: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+  },
+  horario: {
+    type: String,
+    required: true,
+  },
+  aula: {
+    type: String,
+    required: true,
+  },
+  prerrequisitos: {
+    type: String,
+  },
+  cupoMaximo: {
+    type: Number,
+    required: true,
+  },
 });
 
-const Asignatura = mongoose.model("Asignatura", asignaturaSchema);
-
-module.exports = Asignatura;
+module.exports = mongoose.model("Asignatura", asignaturaSchema);
